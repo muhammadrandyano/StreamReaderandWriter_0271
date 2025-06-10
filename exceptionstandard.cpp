1356516c639b228c706606ec1d97818fbdb8ace2
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-06-10 11:13:46 +0700

    membuat int main serta membuat pesan array interger 3 elemen dan memnggil array elemen ke 5

--- exceptionstandard.cpp
+++ exceptionstandard.cpp
@@ -4,3 +4,14 @@
 #include <array>
 // untuk objek array yang akan kita gunakan
 using namespace std;
+
+int main() {
+    cout << "Awal Program" << endl; // penanda 1:...
+    try {
+        array<int, 3> data = {10,20,30};
+        // int data[3] = {10,20,30}
+        //pesan array integer 3 elemen
+        cout<<data.at(5)<<endl;
+        // memanggil array elemen ke 5
+    }
+}
