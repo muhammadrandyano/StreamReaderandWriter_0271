b3b9b24140e4e5c8e8146c080e649b5a1c3eb1da
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-06-10 10:48:26 +0700

    membuat int main dengan membuka file dalam mode menulis serta menunjuk ke sebuah nama file untuk unlimited lopp menulis

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -2,3 +2,21 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main ()
+{
+    string baris;
+    string NamaFile;
+
+    cout << "Masukan Nama File : ";
+    cin >> NamaFile;
+
+    //Membuka file dalam mode menulis.
+    ofstream outFile;
+    //menunjuk ke sebuah nama file .
+    outFile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \' q\' untuk keluar" << endl;
+
+    //unlimited lopp untuk menulis
+}
