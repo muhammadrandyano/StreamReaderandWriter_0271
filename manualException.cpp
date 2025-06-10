6b31fcc9311f9fe01641d069f8021a5416266081
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-06-10 11:09:08 +0700

    membuat blok ini akan dieksekusi

--- manualException.cpp
+++ manualException.cpp
@@ -9,4 +9,10 @@ int main()
         throw 5; // melemparkan sebuah integer maka
         cout << "Pernyataan tidak akan dieksekusi" << endl;
     }
+    catch (int a)
+    {
+        // blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+    catch (...)
 }
